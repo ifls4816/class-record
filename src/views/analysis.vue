c2860413e4e2d1830ce4cc694a99d65c641b2e79
ifls <c4816@foxmail.com>
2023-04-16 21:15:07 +0800

    feat: 代码暂存

--- src/views/analysis.vue
+++ src/views/analysis.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, reactive } from 'vue'
 import * as echarts from 'echarts'
 import { db } from '../server/db/initDB'
 import dayjs from 'dayjs'
@@ -8,15 +8,19 @@ import {
   getDaysArrayByMonth,
   getDaysArrayByWeek,
   calTimeTotal,
-  queryStudentList,
+  queryStudentList
 } from '../utils/common'
 import { Student } from '../types/type'
+
+const state = reactive({
+  currentDate: dayjs()
+})
 
 const classObj = db.get('class')
 const [year, month] = queryYearMonthDay(dayjs())
 // 获取当月记录
 // const monthObj = (classObj ?? classObj[year] ?? classObj[year][month]) || {}
-const monthObj = classObj && classObj[year] && classObj[year][month] || {}
+const monthObj = (classObj && classObj[year] && classObj[year][month]) || {}
 
 // box1
 // 当前周数组
@@ -25,7 +29,12 @@ const weekList = getDaysArrayByWeek()
 let weekValue: number[] = []
 weekList.forEach((values: any) => {
   // const dayData = monthObj[values] ?? 0
-  const dayData = (classObj && classObj[year] && classObj[year][values.m] && classObj[year][values.m][values.d]) ?? 0
+  const dayData =
+    (classObj &&
+      classObj[year] &&
+      classObj[year][values.m] &&
+      classObj[year][values.m][values.d]) ??
+    0
   // 当天有课时情况: 计算课时
   if (dayData.length > 0) {
     weekValue.push(calTimeTotal(dayData) / 60)
@@ -53,11 +62,12 @@ monthXAxis.forEach((values: any) => {
 
 // box3
 const studenList = queryStudentList()
+console.log('studenList', studenList)
 let echartsBoxList: any = []
 studenList.forEach((val: Student) => {
   echartsBoxList.push({
     value: val.frequency,
-    name: val.name,
+    name: val.name
   })
 })
 
@@ -69,7 +79,7 @@ onMounted(() => {
   box1.setOption({
     title: {
       text: '本周课时',
-      left: 'center',
+      left: 'center'
     },
     tooltip: {
       trigger: 'axis',
@@ -77,20 +87,23 @@ onMounted(() => {
         let res = `<span style="color: #f2ca6b">${params[0].name}</span>`
         for (var i = 0; i < params.length; i++) {
           res +=
-            ': <br>' + '共计' + `<span style="color: #de6e6a">${params[i].data}</span>` + '课时'
+            ': <br>' +
+            '共计' +
+            `<span style="color: #de6e6a">${params[i].data}</span>` +
+            '课时'
         }
         return res
-      },
+      }
     },
     xAxis: {
       type: 'category',
-      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
     },
     yAxis: {
       type: 'value',
       axisLabel: {
-        formatter: '{value}h',
-      },
+        formatter: '{value}h'
+      }
     },
     series: [
       {
@@ -98,50 +111,54 @@ onMounted(() => {
         type: 'bar',
         showBackground: true,
         backgroundStyle: {
-          color: 'rgba(68, 142, 247, 0.2)',
-        },
-      },
-    ],
+          color: 'rgba(68, 142, 247, 0.2)'
+        }
+      }
+    ]
   })
 
   box2.setOption({
     title: {
       text: '本月课时',
-      left: 'center',
+      left: 'center'
     },
     tooltip: {
       trigger: 'axis',
       formatter: function (params: any) {
-        var res = '本月' + `<span style="color: #f2ca6b">${params[0].name}</span>`
+        var res =
+          '本月' + `<span style="color: #f2ca6b">${params[0].name}</span>`
         for (var i = 0; i < params.length; i++) {
           res +=
-            '号: <br>' + '共计' + `<span style="color: #de6e6a">${params[i].data}</span>` + '课时'
+            '号: <br>' +
+            '共计' +
+            `<span style="color: #de6e6a">${params[i].data}</span>` +
+            '课时'
         }
         return res
-      },
+      }
     },
     xAxis: {
       type: 'category',
-      data: monthXAxis, // 获取当前月份的每一天 返回数组
+      data: monthXAxis // 获取当前月份的每一天 返回数组
     },
     yAxis: {
       type: 'value',
       axisLabel: {
-        formatter: '{value} h',
-      },
+        formatter: '{value} h'
+      }
     },
     series: [
       {
         data: monthValue,
-        type: 'line',
-      },
-    ],
+        type: 'line'
+      }
+    ]
   })
 
   box3.setOption({
     title: {
       text: '学生课时占比',
-      left: 'center',
+      left: 'center'
     },
     tooltip: {
       trigger: 'item',
@@ -149,15 +166,15 @@ onMounted(() => {
         const {
           data: { name, value },
           color,
-          percent,
+          percent
         } = params
         const html = `<span style="color: ${color}">${name}: </span><br/>累计${value}课时<br/>占比${percent}%`
         return html
-      },
+      }
     },
     legend: {
       orient: 'vertical',
-      left: 'left',
+      left: 'left'
     },
     series: [
       {
@@ -168,15 +185,22 @@ onMounted(() => {
           itemStyle: {
             shadowBlur: 10,
             shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)',
-          },
-        },
-      },
-    ],
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
   })
 })
 </script>
 <template>
+  <div>
+    <a-date-picker
+      class="date-picker"
+      v-model:value="state.currentDate"
+      picker="month"
+    />
+  </div>
   <div class="box">
     <div id="box1"></div>
     <div id="box2">2</div>
@@ -185,6 +209,10 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.date-picker {
+  float: right;
+  margin-bottom: 18px;
+}
 .box {
   display: flex;
   flex-wrap: wrap;
